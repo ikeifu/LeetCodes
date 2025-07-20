b45b1dbc117c848b2f20dd5c96f9e442a1067403
ikeifu <68919302+ikeifu@users.noreply.github.com>
2025-07-20 19:09:55 -0400

    Time: 61 ms (5.67%), Space: 65.1 MB (5.3%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    const dict = {
+        'I' : 1,
+        'V' : 5,
+        'X' : 10,
+        'L' : 50,
+        'C' : 100,
+        'D' : 500,
+        'M' : 1000
+    }
+    let accumulator = 0
+    const romanNum = s.split('').reverse()
+
+    for(let i = 0; i < romanNum.length; i++){
+        const curLet = romanNum[i]
+        const lastLet = romanNum[i-1]
+        if (curLet !== 'I' && curLet !== 'X' && curLet !== 'C'){
+            accumulator += dict[curLet]
+        } else if(lastLet){
+            console.log('hi')
+           dict[curLet] < dict[lastLet] ? accumulator -= dict[curLet] : accumulator += dict[curLet]
+        } else {
+            accumulator += dict[curLet]
+        }
+    }
+    // console.log(accumulator)
+    return accumulator
+};
